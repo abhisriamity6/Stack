663b3aee5477a6c831c217cf5fd0d643362b145d
Administrator <Administrator@ADC-1>
2017-06-17 11:50:55 +0530

    Add project files.

---
+++ Stack/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class StackClass
+    {
+        Char[] StackArray;
+        int sizeofStack;
+        int tos = 0;
+        public StackClass(int size)
+        {
+            sizeofStack = size;
+            StackArray = new char[sizeofStack];
+        }
+        public void push(char element)
+        {
+
+            StackArray[tos++] = element;
+
+
+        }
+        public char pop(char element)
+        {
+
+           return StackArray[tos--];
+
+
+        }
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
